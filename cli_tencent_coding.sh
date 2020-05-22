52c893be306bc7b669c7c830b476eac85e5dee2f
carlos <huaixian.huang@gmail.com>
2020-05-22 16:25:56 +0800

    Feat: script to upload azure tenceentcoding blob account

---
+++ cli_tencent_coding.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export CONNECT_STR='DefaultEndpointsProtocol=https;AccountName=tencentcoding;AccountKey=/EucpM54W6E9Gd0psYD/D4flNJxJTtaLkyVWS/PsrKJKqeC2Ww01ZRIfJSIkj/EyLKCN2gZfkoPqiYwtIaHJpQ==;EndpointSuffix=core.chinacloudapi.cn'
+
+#$1 container name  $2 path $3 file regex
+
+node /Users/carlos/Documents/leju/APPS/carlos/azure-blob-batch-upload-tool/index.js  $1 $2 $3
