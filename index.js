5801db4887682b1519bbc0eb825146edb4039b2e
carlos <huaixian.huang@gmail.com>
2022-06-20 16:16:44 +0800

    Backup upload

--- index.js
+++ index.js
@@ -6,20 +6,32 @@ const _ = require('lodash');
 const path = require('path');
 const uuidv1 = require('uuid/v1');
 const fs = require('fs');
-const readdir = promisify(fs.readdir);
-const stat = promisify(fs.stat);
+// const readdir = promisify(fs.readdir);
+// const stat = promisify(fs.stat);
 
 // require('dotenv').config()
 
+const { readdir } = require('fs').promises;
 
 async function getFiles(dir) {
-  const subdirs = await readdir(dir);
-  const files = await Promise.all(subdirs.map(async (subdir) => {
-    const res = resolve(dir, subdir);
-    return (await stat(res)).isDirectory() ? getFiles(res) : res;
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(dirents.map((dirent) => {
+    const res = resolve(dir, dirent.name);
+    return dirent.isDirectory() ? getFiles(res) : res;
   }));
-  return files.reduce((a, f) => a.concat(f), []);
+  return Array.prototype.concat(...files);
 }
+
+
+
+// async function getFiles(dir) {
+//   const subdirs = await readdir(dir);
+//   const files = await Promise.all(subdirs.map(async (subdir) => {
+//     const res = resolve(dir, subdir);
+//     return (await stat(res)).isDirectory() ? getFiles(res) : res;
+//   }));
+//   return files.reduce((a, f) => a.concat(f), []);
+// }
 
 let [cmd,scirptname,containerName,scan_dir,file_name_regex] = process.argv;
 
@@ -28,6 +40,7 @@ console.log("Dump containerName is:",containerName," scan_dir is:",scan_dir," fi
 async function batch_upload_files(files_list,connect_str,containerName) {
   console.log("Left files to upload is:",files_list.length);
   if (files_list[0]) {
+    console.log("Uploading:",files_list[0]);
     await upload_file_to_auzre(connect_str,containerName,files_list[0])
     return batch_upload_files(_.slice(files_list,1),connect_str,containerName);
   }
@@ -50,5 +63,9 @@ getFiles(scan_dir)
   .then(valid_files=>{
     batch_upload_files(valid_files,process.env.CONNECT_STR,containerName)
   })
-  .catch(e => console.error(e));
+  .catch(e =>{
+      console.error(e)
+      return process.exit(1);
+
+    });
 
